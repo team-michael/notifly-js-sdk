c6ed159d400222f3e617987a2bfd098e30442b6b
minyonglee <rickylee87@gmail.com>
2023-05-01 09:35:46 +0900

    index d ts

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,8 @@
-declare module 'notifly-js-sdk-dev';
+declare module 'notifly-js-sdk-dev' {
+    export function initialize(apiKey: string, options?: any): boolean;
+    export function trackEvent(eventName: string, eventProperties?: any, options?: any): void;
+    export function setUserProperties(userProperties: any, options?: any): void;
+    export function removeUserId(): void;
+    export function setUserId(userId: string, options?: any): void;
+    export function getNotiflyUserID(deviceToken: string): string;
+}
