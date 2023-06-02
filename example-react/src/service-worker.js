b50879e94d92ef39592e0f77470b7eb55cf2b021
minyonglee <rickylee87@gmail.com>
2023-06-02 15:26:54 +0900

    React example app - Service worker

--- example-react/src/service-worker.js
+++ example-react/src/service-worker.js
@@ -70,14 +70,3 @@ self.addEventListener('message', (event) => {
 });
 
 // Any other custom service worker logic can go here.
-
-// Register event listener for the 'push' event.
-self.addEventListener('push', function (event) {
-    // Keep the service worker alive until the notification is created.
-    event.waitUntil(
-        // Show a notification with title 'ServiceWorker Cookbook' and body 'Alea iacta est'.
-        self.registration.showNotification('ServiceWorker Cookbook', {
-            body: 'Alea iacta est',
-        })
-    );
-});
