e00fdd43f091b21ebfbd2351cc87f5538c7a2039
minyonglee <rickylee87@gmail.com>
2023-06-08 09:16:12 +0900

    version up

--- NotiflyServiceWorker.ts
+++ NotiflyServiceWorker.ts
@@ -1,7 +1,7 @@
 // NotiflyServiceWorker.js
 
 // Version of service worker
-const NOTIFLY_SERVICE_WORKER_VERSION = 'v0.4';
+const NOTIFLY_SERVICE_WORKER_VERSION = 'v0.5';
 
 // Installing service worker
 self.addEventListener('install', () => {
