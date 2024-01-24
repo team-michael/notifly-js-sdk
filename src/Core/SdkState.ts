95cd7fe3f1bcb121dfd2da1faf03b61f79f6272c
Junha Park <csjunha@greyboxhq.com>
2024-01-24 12:16:05 +0900

    fix: fix sdk type

--- src/Core/SdkState.ts
+++ src/Core/SdkState.ts
@@ -7,11 +7,7 @@ export enum SdkState {
     FAILED = 3, // SDK failed
 }
 
-export enum SdkType {
-    JS = 'js',
-    JS_CAFE24 = 'js-cafe24',
-    JS_FLUTTER = 'js-flutter',
-}
+export type SdkType = 'js' | 'js-cafe24' | 'js-flutter';
 
 export interface SdkStateObserver {
     onInitialized(): void;
@@ -22,7 +18,7 @@ export interface SdkStateObserver {
 export class SdkStateManager {
     private static _state: SdkState = SdkState.NOT_INITIALIZED;
     private static _observers: SdkStateObserver[] = [];
-    static type: SdkType = SdkType.JS;
+    static type: SdkType = 'js';
     static version: string = SDK_VERSION;
     static source: 'cafe24' | null = null;
 
