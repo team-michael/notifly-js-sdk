babc26dcc871181bc95e5957d945858f6984bed6
Junha Park <csjunha@greyboxhq.com>
2024-04-26 20:45:52 +0900

    feat: remove localforage deps

--- src/Core/Storage/LocalForage.ts
+++
@@ -1,8 +0,0 @@
-import localForage from 'localforage';
-
-export default localForage.createInstance({
-    driver: localForage.INDEXEDDB, // This should be forced to IndexedDB because service worker is using IndexedDB
-    name: 'notifly',
-    storeName: 'notiflyconfig',
-    version: 1.0,
-});
