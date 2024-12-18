3ddea60d7dcb87981cb199563946723a83a74294
Soomin Lee <soomin@greyboxhq.com>
2024-12-18 17:23:48 +0900

    fix: fix path

--- scripts/update_package_name.js
+++ scripts/update_package_name.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
+const path = require('path');
+
+const packageJsonPath = path.resolve(__dirname, '../package.json');
 
 const newName = process.argv[2];
 if (!newName) {
@@ -6,8 +9,8 @@ if (!newName) {
   process.exit(1);
 }
 
-const packageJson = JSON.parse(fs.readFileSync('../package.json', 'utf-8'));
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
 packageJson.name = newName;
 
-fs.writeFileSync('../package.json', JSON.stringify(packageJson, null, 2) + '\n');
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2) + '\n');
 console.log(`Updated package name to "${newName}"`);
